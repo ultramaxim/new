cc17b7e7496ffdbd3105bf307a4758da33bed239
ultramaxim <ultramaximka@gmail.com>
2011-01-08 14:22:40 +0500

    gdt

--- 4.[ASM]ProtectedMode/1.asm
+++ 4.[ASM]ProtectedMode/1.asm
@@ -16,7 +16,14 @@ _1:
 
 	cmp ah,00h
 	jne read_err
-
+	
+	cli
+	lgdt	ptr GDTptr
+	mov	eax,cr0
+	or	al,1
+	mov	cr0,eax
+	sti
+	
 	jmp	read_ok
 read_err:
 	mov	al,1h
@@ -29,7 +36,14 @@ read_err:
 read_ok:
 	cli
 	hlt
-
+	
+GDT:
+  db 8 dup (0)
+  db 0xFF, 0xFF, 0x00, 0x00, 0x00, 10011000b, 00000000b, 0x00
+  db 0x00, 0x10, 0x00, 0x80, 0x0B, 10010010b, 00000000b, 0x00
+  db 0xFF, 0xFF, 0x00, 0x00, 0x00, 10010010b, 00001111b, 0x00
+GDTlen = _1 - GDT
+	
 error db "Error while reading!"
 _2:
 db 510-(_2-_1) dup(0)
