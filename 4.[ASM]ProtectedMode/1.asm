b9dc10d56fd65052cac670ce89dd47c96c3b562b
ultramaxim <ultramaximka@gmail.com>
2011-01-08 14:16:22 +0500

    check error

--- 4.[ASM]ProtectedMode/1.asm
+++ 4.[ASM]ProtectedMode/1.asm
@@ -4,9 +4,10 @@ _1:
 	mov ss,ax
 	mov sp,ax
 	mov es,ax
-
+	mov	ds,sp
+	
 	mov ah,2h
-	mov al,1h
+	mov	al,4h
 	mov ch,0h
 	mov cl,2h
 	mov dh,0h
@@ -16,19 +17,20 @@ _1:
 	cmp ah,00h
 	jne read_err
 
+	jmp	read_ok
 read_err:
-	mov al,1h
-	mov bl,1h
-	mov bh,0h
-	mov cx,_2-error
-	mov bp,error
-	mov ah,13h
-	int 10h
-
+	mov	al,1h
+	mov	bl,1h
+	mov	bh,0h
+	mov	cx,_2-error
+	mov	bp,error
+	mov	ah,13h
+	int	10h
+read_ok:
 	cli
 	hlt
 
-error db "Error while reading!",0h
+error db "Error while reading!"
 _2:
 db 510-(_2-_1) dup(0)
 db 55h,0aah
